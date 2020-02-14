ce2fb417d72ea5ec9e66e443581d37193367f520
Jan Macku <jamacku@redhat.com>
2020-02-14 08:17:00 +0100

    I HAVE NO IDEA WHAT IS WRONG

--- .ci/check-shell.sh
+++ .ci/check-shell.sh
@@ -11,6 +11,9 @@ set -x
 # https://medium.com/@joey_9999/how-to-only-lint-files-a-git-pull-request-modifies-3f02254ec5e0
 # get names of files from PR (excluding deleted files)
 git pull
+git checkout "${TRAVIS_BRANCH}"
+tree .
+git checkout "ci-test-1"
 git diff --name-only --diff-filter=db $(git merge-base HEAD "origin/${TRAVIS_BRANCH}") > ../pr-changes.txt
 
 # Find modified shell scripts
